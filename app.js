fcb9c2704aac2379e8f9aee2be2078d6648a4ea9
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-06-25 20:24:30 +0300

    partials

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
-
+const ejs = require('ejs');
 
 const app = express();
+app.set("view engine", "ejs")
+app.use(express.static("public"));
+
+//ROUTES
+
+app.get("/", (req,res) => {
+    res.render("index", {
+        page_name: "index"
+    });
+})
+app.get("/about", (req,res) => {
+    res.render("about", {
+        page_name: "about"
+    });
+})
+
 
 port = 3000
 app.listen(port, () => {
